d892f122f6f41c5ba525d612b53b786af36d3456
Aanchal Jindal <aanchaljindal@gmail.com>
2013-05-01 16:49:38 -0400

    new sources

---
+++ OpenYouTubeActivity/src/com/aanchal/youtubemysongs/PlaylistId.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.aanchal.youtubemysongs;
+
+
+
+public class PlaylistId extends YouTubeId {
+	public PlaylistId(String pId){
+		super(pId);
+	}
+}
---
+++ OpenYouTubeActivity/src/com/aanchal/youtubemysongs/VideoId.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.aanchal.youtubemysongs;
+
+
+
+public class VideoId extends YouTubeId {
+	public VideoId(String pId){
+		super(pId);
+	}
+}
